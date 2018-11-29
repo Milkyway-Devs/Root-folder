d69af7a1f34adc85de81a4511432662e1fd97c7b
Unstable_Knight <mastertechcraft@gmail.com>
2018-11-29 10:50:39 -0500

    Trying Something

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("twitchchanneltext")
-    .addEventListener("keyup", function (event) {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            document.getElementById("myBtn").click();
-
-        }
-    });
